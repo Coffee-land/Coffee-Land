e2dab0aca4af369a9d3f4b3cffd86062e79357cb
Lawrence AbuDubai <EstarRave203@gmail.com>
2021-08-22 11:46:43 +0300

    Lawrence+Ismael-addcart2

--- js/Cart.js
+++ js/Cart.js
@@ -80,23 +80,23 @@ renderCart();
 
 
 $('.plus').click( function() {
-    var product = $(this).closest('.product')
-    var q = product.data('quantity') + 1;
+    let product = $(this).closest('.product')
+    let q = product.data('quantity') + 1;
     product.data('quantity', q);
     updateProduct(product);
   });
   
   $('.minus').click( function() {
-    var product = $(this).closest('.product')
-    var q = Math.max(1, product.data('quantity') - 1); 
+    let product = $(this).closest('.product')
+    let q = Math.max(1, product.data('quantity') - 1); 
     product.data('quantity', q);
     updateProduct(product);
   });
 
   function updateBill() {
-    var subtotal = 0;
-    var delivery = 5;
-    var total = 0;
+    let subtotal = 0;
+    let delivery = 5;
+    let total = 0;
     $('.product').each(function () {
       subtotal += $(this).data('quantity') * $(this).data('price');
     });
