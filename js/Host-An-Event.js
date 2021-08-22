1a66a9e3c16de8e8ade714a33fadd87d1d3a4264
leenahmad <leenkaraja24@gmail.com>
2021-08-22 16:23:49 +0300

    finish part fome page host with abdulkareem

---
+++ js/Host-An-Event.js
@@ -0,0 +1,7 @@
+'use strict'; 
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+  } 
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  }
