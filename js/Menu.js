2a677cfd3426ea97ceba3d2e61d6a6099166e474
a1abed1a <abedshehadeh1@gmail.com>
2021-08-23 11:41:24 +0300

    fixing erorrs

--- js/Menu.js
+++ js/Menu.js
@@ -6,9 +6,9 @@ let menuEl = document.getElementById("menu");
 let products = []
 // array for cart to save prodact in cart 
 let cart = []
-
+let total = 0;
 // constracter for product 
-function Product(name, price, type){
+function Product(name, price, type) {
     this.name = name;
     this.price = price;
     this.imgurl = `img/${name}.jpg`;
@@ -17,29 +17,36 @@ function Product(name, price, type){
     products.push(this);
 }
 // create prodacts each product have type and price and name 
-new Product('americano-cold', 12,'cold');
-new Product('americano', 12,'hot');
-new Product('bean', 30,'beans');
-new Product('beanBag', 30,'beans');
-new Product('latte', 15,'hot');
-new Product('latteCold', 15,'cold');
+new Product('americano-cold', 12, 'cold');
+new Product('americano', 12, 'hot');
+new Product('bean', 30, 'beans');
+new Product('beanBag', 30, 'beans');
+new Product('latte', 15, 'hot');
+new Product('latteCold', 15, 'cold');
 
 // checking localStorage 
-if(localStorage.getItem('cart')){
-    cart = JSON.parse(localStorage.getItem('cart'));
+function checkingLocalStorage() {
+    if (localStorage.getItem('cart')) {
+        console.log('lol')
+        cart = JSON.parse(localStorage.getItem('cart'));
+    } else {
+        console.log('l');
+        cart = [];
+    }
 }
+checkingLocalStorage();
 // save item in localStorage 
-function pushToStorage(){
-    localStorage.setItem('cart' , JSON.stringify(cart));
+function pushToStorage() {
+    localStorage.setItem('cart', JSON.stringify(cart));
 }
 
 // to render items and menu
-function renderMenu(productType){
+function renderMenu(productType) {
     menuEl.innerHTML = '';
     let ulEl = document.createElement('ul');
     menuEl.appendChild(ulEl);
-    for (let i = 0; i < products.length; i++){
-        if(productType === products[i].type){
+    for (let i = 0; i < products.length; i++) {
+        if (productType === products[i].type) {
             let liEl = document.createElement('li');
             let imgEl = document.createElement('img');
             let inputEl = document.createElement('input');
@@ -53,19 +60,24 @@ function renderMenu(productType){
             inputEl.id = `input${i}`
             inputEl.required;
             buttonEl.textContent = 'Add to cart';
-            buttonEl.addEventListener('click', function(){
+            buttonEl.addEventListener('click', function () {
+                checkingLocalStorage()
                 let boolen = true;
-                for(let j = 0; j < cart.length; j++){
-                    if(products[i].name === cart[j].name){
+                for (let j = 0; j < cart.length; j++) {
+                    if (products[i].name === cart[j].name) {
                         cart[j].quantity = parseInt(cart[j].quantity) + parseInt(document.getElementById(`input${i}`).value);
                         boolen = false;
                     }
                 }
-                if (boolen){
-                cart.push(products[i]);
+                if (boolen) {
+                    products[i].quantity = parseInt(document.getElementById(`input${i}`).value);
+                    cart.push(products[i]);
                 }
                 pushToStorage();
-                inputEl.value = '1';
+                total = 0;
+                for(let x = 0; x < cart.length; x++){
+                    total += cart[x].quantity * cart[x].price;
+                }
             });
             liEl.appendChild(imgEl);
             liEl.appendChild(inputEl);
@@ -74,3 +86,12 @@ function renderMenu(productType){
         }
     }
 }
+renderMenu('hot');
+setInterval(function(){
+    checkingLocalStorage();
+    total = 0;
+    for(let x = 0; x < cart.length; x++){
+        total += cart[x].quantity * cart[x].price;
+    }
+        document.getElementById('total').innerHTML = total;},
+    100);
